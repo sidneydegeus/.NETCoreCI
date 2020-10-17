bafbee8f71b4aec6be3612c0319988a9702ac53b
Sidney de Geus <sidneydegeus@hotmail.com>
2020-10-17 23:03:07 +0200

    Initial commit with a unit test

---
+++ NETCoreCI/CIAPI.Test/Controllers/TestWeatherForecastController.cs
@@ -0,0 +1,33 @@
+﻿using CIAPI.Controllers;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CIAPI.Test.Controllers
+{
+    public class TestWeatherForecastController
+    {
+        readonly WeatherForecastController weatherForecastController = new WeatherForecastController();
+
+        [Fact]
+        public void Get_ShouldReturnForecast_WhenProvidedWithExistingId()
+        {
+            var expected = new WeatherForecast
+            {
+                Date = new DateTime(1992, 8, 20),
+                TemperatureC = 88,
+                Summary = "Bracing"
+            };
+
+            var actual = weatherForecastController.Get(1);
+
+            var expectedStr = JsonConvert.SerializeObject(expected);
+            var actualStr = JsonConvert.SerializeObject(actual);
+
+            Assert.Equal(expectedStr, actualStr);
+        }
+    }
+}
---
+++ NETCoreCI/CIAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CIAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet("{id}")]
+        public WeatherForecast Get(int id)
+        {
+            return new WeatherForecast {
+                Date = new DateTime(1992, 8, 20),
+                TemperatureC = 88,
+                Summary = Summaries[id]
+            };
+        }
+    }
+}
